886acd1ae96a331bdec9ecf745b295aa24e2435d
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-05-18 17:28:18 -0300

    ✅ Added negative image test

---
+++ tests/filters/negative.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "../../libs/Editor.hpp" //! Include Editor header
+
+using namespace std;
+
+//! Main function start
+int main() {
+   //* Definition of variables to create a editor object
+   //? The type is set automatically, however it is still received on input
+   string type { "" };
+   int width { 0 };
+   int height { 0 };
+   int colors { 0 };
+
+   //* Get user data
+   cin >> type;
+   cin >> width >> height;
+   cin >> colors;
+
+   //* Calculate image size and create array with "image size" pixels
+   int size { width * height };
+   Pixel pixels[size];
+
+   for (int i { 0 }; i < size; i++) { //* Repeat "image size" times
+      //^ Definition of variables to create a pixel's objects
+      int red { 0 };
+      int green { 0 };
+      int blue { 0 };
+
+      //^ Get user data
+      cin >> red >> green >> blue;
+
+      //^ Create a pixel and insert it into the array
+      Pixel pixel { red, green, blue, colors };
+      pixels[i] = pixel;
+   }
+
+   //* Create a editor with data
+   Editor editor { width, height, colors, pixels };
+
+   editor.negativeImage(); //* Turn image to gray
+
+   editor.exportData(); //* Export the image output
+} //! End of main function
