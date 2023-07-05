ef282a86a8c8e0d01ba4ba230ce6bc5355fcee7f
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-07-05 16:07:27 -0300

    🚚 Moved files

---
+++ src/classes/Glyph.hpp
@@ -0,0 +1,169 @@
+/**
+ * @file Glyph.hpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Storage and bitmap properties of glyphs.
+ * @version 2.0
+ * @date 2023-07-03
+ *
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef GLYPH_HPP
+#define GLYPH_HPP
+
+#include <iostream>
+#include <fstream>
+
+/**
+ * @class Glyph class.
+ * @author André Maurício (andre@dimap.ufrn.br)
+ * @brief Glyph definition with attributes and methods.
+ */
+class Glyph {
+public:
+   /**
+    * @brief Construct a new Glyph object.
+    */
+   Glyph() : 
+      encoding(0), 
+      width(0), 
+      height(0), 
+      offset_x(0), 
+      offset_y(0), 
+      device_width_x(0), 
+      device_width_y(0), 
+      bitmap(nullptr) { }
+
+   /**
+    * @brief Destroy the Glyph object.
+    */
+   ~Glyph() {
+      if (bitmap != nullptr) {
+         delete[] this->bitmap;
+      }
+   }
+
+   /**
+    * @brief Read a Glyph from the BDF file.
+    *
+    * @param file BDF file.
+    */
+   void read(std::istream& file) {
+      int swx0, swy0;
+      int xoff, yoff;
+
+      std::string prop = "";
+      file >> this->name;
+
+      while (prop != "ENDCHAR") {
+         if (prop == "ENCODING") {
+            file >> encoding;
+         } else if (prop == "SWIDTH" || prop == "SWIDTH1") {
+            file >> swx0 >> swy0;
+         } else if (prop == "DWIDTH" || prop == "DWIDTH1") {
+            file >> device_width_x >> device_width_y;
+         } else if (prop == "VVECTOR") {
+            file >> xoff >> yoff;
+         } else if (prop == "BBX") {
+            file >> width >> height >> offset_x >> offset_y;
+         } else if (prop == "BITMAP") {
+            std::string line;
+            int size = width > 8 ? 16 : 8;
+            this->bitmap = new bool[width * height];
+
+            file.ignore();
+
+            for (int i = 0; i < height; i++) {
+               unsigned int v;
+               getline(file, line);
+               sscanf(line.c_str(), "%x", &v);
+
+               for (int j = 0; j < width; j++) {
+                  unsigned long long mask = 1 << (size - j - 1);
+                  bool isOn = (v & mask) != 0;
+                  this->bitmap[i * width + j] = isOn;
+               }
+            }
+         }
+
+         file >> prop;
+      }
+   }
+
+   /**
+    * @brief Get the name of the glyph.
+    * 
+    * @return An string.
+    */
+   std::string getName() const { return name; }
+
+   /**
+    * @brief Get the encoding of the glyph.
+    * 
+    * @return An integer. 
+    */
+   int getEncoding() const { return encoding; }
+
+   /**
+    * @brief Get the width of the width.
+    * 
+    * @return An integer. 
+    */
+   int getWidth() const { return width; }
+
+   /**
+    * @brief Get the height of the glyph.
+    * 
+    * @return An integer. 
+    */
+   int getHeight() const { return height; }
+
+   /**
+    * @brief Get the x offset of the glyph.
+    * 
+    * @return An integer. 
+    */
+   int getXOffset() const { return offset_x; }
+
+   /**
+    * @brief Get the y offset of the glyph.
+    * 
+    * @return An integer. 
+    */
+   int getYOffset() const { return offset_y; }
+
+   /**
+    * @brief Get the device width x of the glyph.
+    * 
+    * @return An integer. 
+    */
+   int getDeviceWidthX() const { return device_width_x; }
+   /**
+    * @brief Get the device width y of the glyph.
+    * 
+    * @return An integer. 
+    */
+   int getDeviceWidthY() const { return device_width_y; }
+
+   /**
+    * @brief Checks if a glyph bit is on or off.
+    * 
+    * @param x X coordinate of the bit.
+    * @param y Y coordinate of the bit
+    * @return An boolean.
+    */
+   bool getBit(int x, int y) const { return bitmap[y * width + x]; }
+
+private:
+   std::string name;   /**< Name */
+   int encoding;       /**< Encoding */
+   int width;          /**< Bitmap width */
+   int height;         /**< Bitmap height */
+   int offset_x;       /**< Offset X */
+   int offset_y;       /**< Offset Y */
+   int device_width_x; /**< Device width X */
+   int device_width_y; /**< Device width Y */
+   bool* bitmap;       /**< Bitmap */
+};
+
+#endif // GLYPH_HPP
---
+++ src/utils/Helpers.hpp
@@ -0,0 +1,143 @@
+/**
+ * @file Helpers.hpp
+ * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
+ * @brief Help functions.
+ * @version 2.0
+ * @date 2023-06-25
+ * 
+ * Defines the help functions, either of the program in general or of a
+ * specific parameter.
+ * 
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef HELPERS_HPP
+#define HELPERS_HPP
+
+#include <iostream>
+
+/**
+ * @brief Print the tutorial on how to use the program.
+ */
+void printHelp() {
+   std::cout << "Usage: ppmedit [OPTION]... -i [FILE] -o [OUTPUT_FILE]" 
+      << std::endl;
+   std::cout << "Edit ppm images and save to the specified file." << std::endl;
+
+   std::cout << std::endl;
+
+   std::cout << "Without an OUTPUT_FILE it saves to \"a.ppm\"." << std::endl;
+
+   std::cout << std::endl;
+
+   std::cout << "\t-b, --border\t\tsize of border." << std::endl;
+   std::cout << "\t-e, --effect\t\teffect to be applied." << std::endl;
+   std::cout << "\t-f, --font\t\tfont name for writing." << std::endl;
+   std::cout << "\t-F, --foreground\tforeground in image combination." 
+      << std::endl;
+   std::cout << "\t-h, --help\t\thelp command." << std::endl;
+   std::cout << "\t-i, --input\t\tinput image." << std::endl;
+   std::cout << "\t-l, --length\t\tfont length." << std::endl;
+   std::cout << "\t-o, --output\t\toutput image." << std::endl;
+   std::cout << "\t-s, --space\t\tadditional bottom border space." << std::endl;
+   std::cout << "\t-t, --text\t\ttext to overlay image." << std::endl;
+
+   std::cout << std::endl;
+
+   std::cout << "Examples:" << std::endl;
+   std::cout << "\tppmedit -e gray -i image.ppm\t\t\tadd the gray effect to" 
+      << "image.ppm and save to a.ppm." << std::endl;
+   std::cout << "\tppmedit -e polaroid -s 25 -i i.ppm -o p.ppm\t" 
+      << "add the polaroid border effect to i.ppm with additional space below" 
+      << "25 pixels and save to p.ppm." << std::endl;
+}
+
+/**
+ * @brief Print the tutorial on how to use the border option.
+ */
+void printBorderHelp() {
+   std::cout << "Usage: ppmedit [OPTION]... -i [INPUT_FILE] -o [OUTPUT_FILE]" 
+   << std::endl;
+   std::cout << "Edit ppm images and save to the specified file." << std::endl;
+
+   std::cout << std::endl;
+
+   std::cout << "Without an OUTPUT_FILE it saves to \"a.ppm\"." << std::endl;
+   std::cout << "Without setting the \"-b\" option, but using the solid " 
+      << "border or polaroid effect, the default value is 25(px). \"a.ppm\"." 
+      << std::endl;
+
+   std::cout << std::endl;
+
+   std::cout << "\t-b, --border\t\tsize of border." << std::endl;
+   std::cout << "Works in conjunction with the solid edge and Polaroid "
+      << "edge effects." << std::endl;
+   std::cout << "It defines the size of the borders in pixels or intensity." 
+      << std::endl;
+   std::cout << "It is accompanied by an integer value (number of pixels) or "
+      << "a string (slim, normal or large)." << std::endl;
+
+   std::cout << std::endl;
+
+   std::cout << "Examples:" << std::endl;
+   std::cout << "\tppmedit -e b-solid -b 50 -i i.ppm\t\t\tadd a solid border "
+      << "to the image with size 50(px)." << std::endl;
+}
+
+/**
+ * @brief Print the tutorial on how to use the effect option.
+ * 
+ * @todo Set the help information for the effects option.
+ */
+void printEffectHelp() { }
+
+/**
+ * @brief Print the tutorial on how to use the font option.
+ * 
+ * @todo Set the help information for the font option.
+ */
+void printFontHelp() { }
+
+/**
+ * @brief Print the tutorial on how to use the foreground option.
+ * 
+ * @todo Set the help information for the foreground option.
+ */
+void printForegroundHelp() { }
+
+/**
+ * @brief Print the tutorial on how to use the input option.
+ * 
+ * @todo Set the help information for the input option.
+ */
+void printInputHelp() { }
+
+/**
+ * @brief Print the tutorial on how to use the length option.
+ * 
+ * @todo Set the help information for the length option.
+ */
+void printLengthHelp() { }
+
+/**
+ * @brief Print the tutorial on how to use the output option.
+ * 
+ * @todo Set the help information for the output option.
+ */
+void printOutputHelp() { }
+
+/**
+ * @brief Print the tutorial on how to use the space option.
+ * 
+ * @todo Set the help information for the space option.
+ */
+void printSpaceHelp() { }
+
+/**
+ * @brief Print the tutorial on how to use the text option.
+ * 
+ * @todo Set the help information for the text option.
+ */
+void printTextHelp() { }
+
+#endif // HELPERS_HPP
