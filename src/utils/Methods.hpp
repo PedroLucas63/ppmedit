982c39c2c010229468ac36f0e138948609215a1c
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-07-18 11:23:12 -0300

    👷 Addition of the writing method

--- src/utils/Methods.hpp
+++ src/utils/Methods.hpp
@@ -3,7 +3,7 @@
  * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
  * @brief Methods functions.
  * @version 2.0
- * @date 2023-07-07
+ * @date 2023-07-18
  * 
  * Manipulates methods in the editor.
  * 
@@ -15,111 +15,6 @@
 
 #include <iostream>
 #include "Editor.hpp"
-
-/**
- * @enum Effects enumeration.
- * @brief Enumeration with all effects of the "Editor" class.
- */
-enum Effects {
-   Blurring,
-   Edge_Sharpening,
-   Embossing,
-   Enlarge,
-   Gray,
-   Invert,
-   Negative,
-   Reduce,
-   Rotate_Left,
-   Rotate_Right,
-   Sharpening,
-   Effect_None
-};
-
-/**
- * @enum Borders enumeration.
- * @brief Enumeration with all borders of the "Editor" class.
- */
-enum Borders {
-   Solid,
-   Polaroid,
-   Border_None
-};
-
-/**
- * @enum Types enumeration.
- * @brief Enumeration with all types of the "Editor" class.
- */
-enum Types {
-   Text,
-   Binary,
-   Automatic
-};
-
-/**
- * @brief Get the value from Effects enum by effect name.
- * 
- * @param effect_name Effect name.
- * @return An Effects value.
- */
-Effects getEffectByName(std::string effect_name) {
-   if (effect_name == "gray") {
-      return Gray;
-   } else if (effect_name == "negative") {
-      return Negative;
-   } else if (effect_name == "rotate") {
-      return Rotate_Right;
-   } else if (effect_name == "rotate-l") {
-      return Rotate_Left;
-   } else if (effect_name == "invert") {
-      return Invert;
-   } else if (effect_name == "enlarge") {
-      return Enlarge;
-   } else if (effect_name == "reduce") {
-      return Reduce;
-   } else if (effect_name == "sharp") {
-      return Sharpening;
-   } else if (effect_name == "sharp-e") {
-      return Edge_Sharpening;
-   } else if (effect_name == "blur") {
-      return Blurring;
-   } else if (effect_name == "embossing") {
-      return Embossing;
-   }
-
-   return Effect_None;
-}
-
-/**
- * @brief Get the value from Borders enum by border type.
- * 
- * @param borders_type Border type.
- * @return An Borders value.
- */
-Borders getBorderByType(std::string borders_type) {
-   if (borders_type == "polaroid") {
-      return Polaroid;
-   } else if (borders_type == "solid") {
-      return Solid;
-   }
-
-   return Border_None;
-}
-
-/**
- * @brief Get the value from type enum.
- * 
- * @param type_to_convert Type to convert.
- * @return An Type value.
- */
-Types getTypeByValue(std::string type_to_convert) {
-   if (type_to_convert == "text") {
-      return Text;
-   } else if (type_to_convert == "binary") {
-      return Binary;
-   }
-
-   return Automatic;
-}
 
 /**
  * @brief Performs the image blending function
@@ -279,6 +174,7 @@ int getBorderSize(std::string size, int image_width, int image_height) {
  * @param size Border size.
  * @param extra_size Extra size underneath.
  * @param color Border color.
+ * @see getBorderSize()
  */
 template <
    typename T,
@@ -313,9 +209,45 @@ void setBorder(Editor& editor, Borders type, T size,
 }
 
 /**
+ * @brief Separates text data into pairs and uses the editor method.
+ * 
+ * @param editor Editor memory position.
+ * @param text Text to write.
+ * @param x Displacement in X axis.
+ * @param y Displacement in Y axis.
+ * @param font_name Font name.
+ */
+void setText(Editor& editor, std::string text, int x, int y,
+   std::string font_name) 
+{
+   Font const& font { getFontByName(font_name) };
+
+   std::string word;
+   std::vector<Pair<TypeText, std::string>> mapped_text;
+
+   for (auto c : text) {
+      if (c == ' ') {
+         Pair<TypeText, std::string> pair { getTypeByText(word), word };
+         mapped_text.push_back(pair);
+
+         word = "";
+      } else {
+         word += c;
+      }
+   }
+
+   if (word != "") {
+      Pair<TypeText, std::string> pair { getTypeByText(word), word };
+      mapped_text.push_back(pair);
+   }
+
+   editor.writeText(mapped_text, x, y, font);
+}
+
+/**
  * @brief Performs image conversion.
  * 
- * @param Editor memory position.
+ * @param editor Editor memory position.
  * @param type Type to convert.
  */
 void performConversion(Editor& editor, Types type) {
